f1aa11e5157e0685b2333e2ece9ed1e6e77da1be
Flash <109522839+DHIRAJ-GHOLAP@users.noreply.github.com>
2024-08-27 17:30:43 +0530

    remove other unessry func
    this is one my my first disocrd bot code so pls just dont judge it its litrally my first discord bot code
    i updated code and add bot token to config.json file so go and edit the value there

--- bot.py
+++ bot.py
@@ -1,72 +1,95 @@
+print('change the info in config.json file and if you have any queries the join my server link - discord.gg/invites/homies-here')
 import time
 import discord
 from discord.ext import commands
-import Confidential
+import json  
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+
+
+bot_token = config['BOT_TOKEN']
+owner_usr_id = config['OWNER_USER_ID']
+founder_id = config['FOUNDER_ID']
+gc = config['GC_CHANNEL_ID']
+admins = config['ADMINS']
+compliments = config['COMPLIMENTS']
+log = config['logchannel']
+
+
+
 intents = discord.Intents.default()
-intents.members = True  
-
-bot_token = Confidential.token
-owner_usr_id = 738739339969822730
-gali_counter = []
-founder_id = 1141700782027194470
-
+intents.members = True
 
 class MyBot(discord.Client):
-   async def on_ready(self):
-      print(f'Logged in as {self.user} (ID: {self.user.id})')
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
 
-   async def on_message(self, message):
-      try:
-         founder = await self.fetch_user(founder_id)
-         if message.author == self.user:  # Prevent bot from replying to itself
-            return
-         bot_owner = await self.fetch_user(owner_usr_id) 
-         message_id = message.id
-         channel = self.get_channel(message.channel.id)  # Assuming you have the channel ID
-         fetched_message = await channel.fetch_message(message_id)
-         dm_channel = await bot_owner.create_dm()
-         founder_dm_channel = await founder.create_dm()
-         if ('https://tenor.com' ) in fetched_message.content  :  # This will ignore the GIF links
-            pass
-         elif f'<@{owner_usr_id}>' in fetched_message.content : 
-               print ("Owner has tagged")
-               dm_message = f"**Hey!**"
-               channel = self.get_channel(message.channel.id)  # Assuming you have the channel ID
-               fetched_message = await channel.fetch_message(message_id)
-               print(f" {fetched_message.content}")
-               
-               dm_message += f" You were mentioned by {message.author.mention} in {message.channel.name}: TIME = {time.strftime(' %I:%M %p')} {fetched_message.clean_content} \n"
-               await dm_channel.send(dm_message) 
-               print(message.author.name," - ",fetched_message.clean_content )
-         
-         if (message.author.name == 'flash.homies') and (fetched_message.clean_content == '!b'):
-            await fetched_message.delete()
-            time.sleep(3)
-            for i in range(20):
-               await message.channel.send("I am a suside bomber")
-         if (message.author.name == founder or bot_owner)and(fetched_message.clean_content.startswith('create staff')):
-            founder_dm_message = '! '
-            founder_dm_message += f"Sucessfully assign role to "
-            cm = fetched_message.content
-            role_user = cm[15:-1]
-            assigning = await self.fetch_user(role_user)
-            
-            # await founder_dm_channel.send(founder_dm_channel) 
-            role = discord.utils.get(message.guild.roles, name="Staff")  # Replace "Member" with the actual role name
-            if role:
-                await assigning.add_role(role)
-                await message.channel.send(f"Role '{role.name}' assigned to {await self.fetch_user(role_user)}")
-            else:
-                await message.channel.send("Role not found.")
-      except Exception as e:
-         print("something went wrong")
-         print(e)
-         
+    async def on_message(self, message):
+        try:
+            ###########################################################################
+            ##################### Main code ###########################################
+            ###########################################################################
+            bot_owner = await self.fetch_user(owner_usr_id)
+            founder = await self.fetch_user(founder_id)
 
+            if message.author == self.user:  
+                return
 
+            message_id = message.id
+            channel = self.get_channel(message.channel.id) 
+            fetched_message = await channel.fetch_message(message_id)
+            dm_channel = await bot_owner.create_dm()
+            founder_dm_channel = await founder.create_dm()
 
+            if ('https://tenor.com' ) in fetched_message.content:  
+                pass
+
+            elif (message.author.name in admins) and (fetched_message.clean_content.startswith('!m')):
+                members = [member.name for member in message.guild.members]  
+                with open("members.txt", "w", encoding="utf-8") as mem:  
+                    mem.write("\n".join(members))  
+                await message.channel.send(file=discord.File('members.txt'))
+
+            elif f'<@{owner_usr_id}>' in fetched_message.content:
+                print("Owner has tagged")
+                dm_message = f"**Hey!**"
+                channel = self.get_channel(message.channel.id) 
+                fetched_message = await channel.fetch_message(message_id)
+                dm_message += f" You were mentioned by {message.author.mention} in {message.channel.name}: TIME = {time.strftime(' %I:%M %p')} {fetched_message.clean_content} \n"
+                await dm_channel.send(dm_message)
+                print(message.author.name, " - ", fetched_message.clean_content)
+
+            if fetched_message.clean_content.startswith('?b'):
+                if '?ban ' in fetched_message.clean_content:
+                    # await fetched_message.delete()
+                    await message.channel.send(f"Abe ja na {message.author.mention}, \n ")
+                    await message.channel.send(f"https://tenor.com/view/mera-lavda-le-bhau-gif-19383216")
+
+                else:
+                    embedVar = discord.Embed(
+                        title="Command: ?ban",
+                        description="Description: Ban a member, optional time limit\nCooldown: 3 seconds",
+                        color=0x4d4dff
+                    )
+                    embedVar.add_field(
+                        name="Usage:",
+                        value="?ban [user] [limit] [reason]\n?ban save [user] [limit] [reason]\n?ban noappeal [user] [limit] [reason]",
+                        inline=False
+                    )
+                    embedVar.add_field(
+                        name="Example:",
+                        value="?ban Flash making bugs\n?ban Flash 2d needs to calm down\n?ban noappeal Flash dont come back",
+                        inline=False
+                    )
+                    await message.channel.send(embed=embedVar)
+
+        except Exception as e:
+            channel = self.get_channel(log)
+            await channel.send(f"Error: {e}")
+            print("something went wrong")
+            print(e)
 
 client = MyBot(intents=intents)
 client.run(bot_token)
-
-
